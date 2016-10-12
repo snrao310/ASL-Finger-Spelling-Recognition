d35a22a16c2357e1801274f4285692bddeed7039
snrao310 <sameerana.nrao@yahoo.com>
2016-10-12 15:50:54 -0700

    grayscale conversion for faster training and classifying

--- main.py
+++ main.py
@@ -3,28 +3,56 @@
 import cv2
 import numpy
 import math
+import os
 
-#todo: Ashish fill this
-def identifyGesture(handImage):
+paths = ['/home/snrao/IDE/PycharmProjects/ASL Finger Spelling Recognition']
+TOTAL_DATASET = 2515
+x_train = [TOTAL_DATASET]
+y_train = [TOTAL_DATASET]
+
+
+def load_data_set():
+    for path in paths:
+        for root, directories, filenames in os.walk(path):
+            for filename in filenames:
+                if filename.endswith(".jpeg"):
+                    fullpath = os.path.join(root, filename)
+                    im = cv2.imread(fullpath)
+                    im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+                    x_train.append(im)
+                    t = fullpath.rindex('/')
+                    fullpath = fullpath[0:t]
+                    n = fullpath.rindex('/')
+                    print(fullpath[n + 1:t])
+                    y_train.append(fullpath[n + 1:t])
+
+
+# todo: Ashish fill this
+def identifyGesture(handTrainImage):
+    handTrainImage=cv2.cvtColor(handTrainImage,cv2.COLOR_BGR2GRAY)
     # currently returns gesture detected string. needs to return the actual letter it detects.
     # Or if it doesn't detect anything, then it should return null string.
     return "Gesture Detected"
+
 
 def nothing(x):
     pass
 
 
+load_data_set()
+
 # Create a window to display the camera feed
 cv2.namedWindow('Camera Output')
 cv2.namedWindow('Hand')
+cv2.namedWindow('HandTrain')
 
 # TrackBars for fixing skin color of the person
-cv2.createTrackbar('B for min','Camera Output',0,255,nothing)
-cv2.createTrackbar('G for min','Camera Output',0,255,nothing)
-cv2.createTrackbar('R for min','Camera Output',0,255,nothing)
-cv2.createTrackbar('B for max','Camera Output',0,255,nothing)
-cv2.createTrackbar('G for max','Camera Output',0,255,nothing)
-cv2.createTrackbar('R for max','Camera Output',0,255,nothing)
+cv2.createTrackbar('B for min', 'Camera Output', 0, 255, nothing)
+cv2.createTrackbar('G for min', 'Camera Output', 0, 255, nothing)
+cv2.createTrackbar('R for min', 'Camera Output', 0, 255, nothing)
+cv2.createTrackbar('B for max', 'Camera Output', 0, 255, nothing)
+cv2.createTrackbar('G for max', 'Camera Output', 0, 255, nothing)
+cv2.createTrackbar('R for max', 'Camera Output', 0, 255, nothing)
 
 # Default skin color values in natural lighting
 # cv2.setTrackbarPos('B for min','Camera Output',52)
@@ -35,28 +63,28 @@ cv2.createTrackbar('R for max','Camera Output',0,255,nothing)
 # cv2.setTrackbarPos('R for max','Camera Output',146)
 
 # Default skin color values in indoor lighting
-cv2.setTrackbarPos('B for min','Camera Output',0)
-cv2.setTrackbarPos('G for min','Camera Output',130)
-cv2.setTrackbarPos('R for min','Camera Output',103)
-cv2.setTrackbarPos('B for max','Camera Output',255)
-cv2.setTrackbarPos('G for max','Camera Output',182)
-cv2.setTrackbarPos('R for max','Camera Output',130)
+cv2.setTrackbarPos('B for min', 'Camera Output', 0)
+cv2.setTrackbarPos('G for min', 'Camera Output', 130)
+cv2.setTrackbarPos('R for min', 'Camera Output', 103)
+cv2.setTrackbarPos('B for max', 'Camera Output', 255)
+cv2.setTrackbarPos('G for max', 'Camera Output', 182)
+cv2.setTrackbarPos('R for max', 'Camera Output', 130)
 
 # Get pointer to video frames from primary device
 videoFrame = cv2.VideoCapture(0)
 
 # Process the video frames
-keyPressed = -1 # -1 indicates no key pressed. Can press any key to exit
+keyPressed = -1  # -1 indicates no key pressed. Can press any key to exit
 
 # cascade xml file for detecting palm. Haar classifier
 palm_cascade = cv2.CascadeClassifier('palm.xml')
 
 # previous values of cropped variable
-x_crop_prev, y_crop_prev, w_crop_prev, h_crop_prev= 0, 0, 0, 0
+x_crop_prev, y_crop_prev, w_crop_prev, h_crop_prev = 0, 0, 0, 0
 
 # previous cropped frame if we need to compare histograms of previous image with this to see the change.
 # Not used but may need later.
-_, prevHandImage=videoFrame.read()
+_, prevHandImage = videoFrame.read()
 
 # previous frame contour of hand. Used to compare with new contour to find if gesture has changed.
 prevcnt = numpy.array([], dtype=numpy.int32)
@@ -64,34 +92,33 @@ prevcnt = numpy.array([], dtype=numpy.int32)
 # gesture static increments when gesture doesn't change till it reaches 10 (frames) and then resets to 0.
 # gesture detected is set to 10 when gesture static reaches 10."Gesture Detected is displayed for next
 # 10 frames till gestureDetected decrements to 0.
-gestureStatic=0
-gestureDetected=0
+gestureStatic = 0
+gestureDetected = 0
 
-while keyPressed < 0: # any key pressed has a value >= 0
+while keyPressed < 0:  # any key pressed has a value >= 0
 
     # Getting min and max colors for skin
-    min_YCrCb = numpy.array([cv2.getTrackbarPos('B for min','Camera Output'),
-                             cv2.getTrackbarPos('G for min','Camera Output'),
-                             cv2.getTrackbarPos('R for min','Camera Output')],numpy.uint8)
-    max_YCrCb = numpy.array([cv2.getTrackbarPos('B for max','Camera Output'),
-                             cv2.getTrackbarPos('G for max','Camera Output'),
-                             cv2.getTrackbarPos('R for max','Camera Output')],numpy.uint8)
+    min_YCrCb = numpy.array([cv2.getTrackbarPos('B for min', 'Camera Output'),
+                             cv2.getTrackbarPos('G for min', 'Camera Output'),
+                             cv2.getTrackbarPos('R for min', 'Camera Output')], numpy.uint8)
+    max_YCrCb = numpy.array([cv2.getTrackbarPos('B for max', 'Camera Output'),
+                             cv2.getTrackbarPos('G for max', 'Camera Output'),
+                             cv2.getTrackbarPos('R for max', 'Camera Output')], numpy.uint8)
 
     # Grab video frame, Decode it and return next video frame
     readSucsess, sourceImage = videoFrame.read()
 
     # Convert image to YCrCb
-    imageYCrCb = cv2.cvtColor(sourceImage,cv2.COLOR_BGR2YCR_CB)
+    imageYCrCb = cv2.cvtColor(sourceImage, cv2.COLOR_BGR2YCR_CB)
 
     # Find region with skin tone in YCrCb image
-    skinRegion = cv2.inRange(imageYCrCb,min_YCrCb,max_YCrCb)
+    skinRegion = cv2.inRange(imageYCrCb, min_YCrCb, max_YCrCb)
 
     # Do contour detection on skin region
-    _,contours, hierarchy = cv2.findContours(skinRegion, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    _, contours, hierarchy = cv2.findContours(skinRegion, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    # sorting contours by area. Largest area first and drawing the largest contour.
+    # sorting contours by area. Largest area first.
     contours = sorted(contours, key=cv2.contourArea, reverse=True)
-    cv2.drawContours(sourceImage, contours, 0, (0, 255, 0), 1)
 
     # get largest contour and compare with largest contour from previous frame.
     # set previous contour to this one after comparison.
@@ -99,23 +126,29 @@ while keyPressed < 0: # any key pressed has a value >= 0
     ret = cv2.matchShapes(cnt, prevcnt, 2, 0.0)
     prevcnt = contours[0]
 
+    # once we get contour, extract it without background into a new window called handTrainImage
+    stencil = numpy.zeros(sourceImage.shape).astype(sourceImage.dtype)
+    color = [255, 255, 255]
+    cv2.fillPoly(stencil, [cnt], color)
+    handTrainImage = cv2.bitwise_and(sourceImage, stencil)
+
     # if comparison returns a high value (shapes are different), start gestureStatic over. Else increment it.
-    if(ret>0.70):
-        gestureStatic=0
+    if (ret > 0.70):
+        gestureStatic = 0
     else:
-        gestureStatic+=1
+        gestureStatic += 1
 
     # if gesture is static for 10 frames, set gestureDetected to 10 and display "gesture detected"
     # on screen for 10 frames.
-    if gestureStatic==10:
-        gestureDetected=10;
+    if gestureStatic == 10:
+        gestureDetected = 10;
         print("Gesture Detected")
-        letterDetected=identifyGesture(handImage) #todo: Ashish fill this function to return actual character
+        letterDetected = identifyGesture(handTrainImage)  # todo: Ashish fill this function to return actual character
 
-    if gestureDetected>0:
-        if(letterDetected!= None):
+    if gestureDetected > 0:
+        if (letterDetected != None):
             cv2.putText(sourceImage, letterDetected, (10, 400), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 2)
-        gestureDetected-=1
+        gestureDetected -= 1
 
     # crop coordinates for hand.
     x_crop, y_crop, w_crop, h_crop = cv2.boundingRect(cnt)
@@ -124,15 +157,20 @@ while keyPressed < 0: # any key pressed has a value >= 0
     cv2.rectangle(sourceImage, (x_crop, y_crop), (x_crop + w_crop, y_crop + h_crop), (0, 255, 0), 2)
 
     # if the crop area has changed drastically form previous frame, update it.
-    if(abs(x_crop-x_crop_prev)>50 or abs(y_crop-y_crop_prev)>50 or
-               abs(w_crop-w_crop_prev)>50 or abs(h_crop-h_crop_prev)>50):
-        x_crop_prev=x_crop
-        y_crop_prev=y_crop
-        h_crop_prev=h_crop
-        w_crop_prev=w_crop
+    if (abs(x_crop - x_crop_prev) > 50 or abs(y_crop - y_crop_prev) > 50 or
+                abs(w_crop - w_crop_prev) > 50 or abs(h_crop - h_crop_prev) > 50):
+        x_crop_prev = x_crop
+        y_crop_prev = y_crop
+        h_crop_prev = h_crop
+        w_crop_prev = w_crop
 
     # create crop image
-    handImage = sourceImage[max(0, y_crop_prev - 50):y_crop_prev + h_crop_prev + 50, max(0, x_crop_prev - 50):x_crop_prev + w_crop_prev + 50]
+    handImage = sourceImage.copy()[max(0, y_crop_prev - 50):y_crop_prev + h_crop_prev + 50,
+                max(0, x_crop_prev - 50):x_crop_prev + w_crop_prev + 50]
+
+    # Training image with black background
+    handTrainImage = handTrainImage[max(0, y_crop_prev - 15):y_crop_prev + h_crop_prev + 15,
+                     max(0, x_crop_prev - 15):x_crop_prev + w_crop_prev + 15]
 
     # Comparing histograms of this image and previous image to check if the gesture has changed.
     # Not accurate. So switched to contour comparisons.
@@ -167,26 +205,31 @@ while keyPressed < 0: # any key pressed has a value >= 0
         start = tuple(cnt[s][0])
         end = tuple(cnt[e][0])
         far = tuple(cnt[f][0])
-        if count_defects==0:
-            center_of_palm=far
+        if count_defects == 0:
+            center_of_palm = far
         a = math.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
         b = math.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
         c = math.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
         angle = math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c)) * 57
         if angle <= 90:
             count_defects += 1
-            if count_defects<5:
+            if count_defects < 5:
                 # cv2.circle(sourceImage, far, 5, [0, 0, 255], -1)
                 center_of_palm = (far[0] + center_of_palm[0]) / 2, (far[1] + center_of_palm[1]) / 2
         cv2.line(sourceImage, start, end, [0, 255, 0], 2)
     # cv2.circle(sourceImage, avr, 10, [255, 255, 255], -1)
 
+
+    # drawing the largest contour
+    cv2.drawContours(sourceImage, contours, 0, (0, 255, 0), 1)
+
     # Display the source image and cropped image
-    cv2.imshow('Camera Output',sourceImage)
+    cv2.imshow('Camera Output', sourceImage)
     cv2.imshow('Hand', handImage)
+    cv2.imshow('HandTrain', handTrainImage)
 
     # Check for user input to close program
-    keyPressed = cv2.waitKey(30) # wait 30 miliseconds in each iteration of while loop
+    keyPressed = cv2.waitKey(30)  # wait 30 miliseconds in each iteration of while loop
 
 # Close window and camera after exiting the while loop
 cv2.destroyWindow('Camera Output')
